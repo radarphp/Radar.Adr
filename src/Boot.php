1164afa0bb3c07df467e368c346af769a67cfd5d
Paul M. Jones <pmjones88@gmail.com>
2015-05-07 13:06:16 -0500

    an attempt at container caching

--- src/Boot.php
+++ src/Boot.php
@@ -20,9 +20,30 @@ class Boot
         $loader->parse();
         $loader->toEnv();
 
-        $config = array_merge(['Radar\Adr\Config'], $config);
-        $di = (new ContainerBuilder())->newConfiguredInstance([], $config);
+        if (! empty($_ENV['RADAR_ADR_CONTAINER_CACHE'])) {
+            $di = $this->cachedContainer($config);
+        } else {
+            $di = $this->newContainer($config);
+        }
 
         return $di->newInstance('Radar\Adr\Adr');
+    }
+
+    protected function cachedContainer(array $config)
+    {
+        $file = $_ENV['RADAR_ADR_CONTAINER_CACHE'];
+        if (file_exists($file)) {
+            return unserialize(file_get_contents($file));
+        }
+
+        $di = $this->newContainer($config);
+        file_put_contents($file, serialize($di));
+        return $di;
+    }
+
+    protected function newContainer(array $config)
+    {
+        $config = array_merge(['Radar\Adr\Config'], $config);
+        return (new ContainerBuilder())->newConfiguredInstance([], $config);
     }
 }
