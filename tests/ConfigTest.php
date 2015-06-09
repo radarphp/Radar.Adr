e6807e86dd16be41d381f47bc460b05cc40c7771
Paul M. Jones <pmjones88@gmail.com>
2015-06-09 16:23:53 -0500

    use Relay for ResponseSender and ExceptionHandler

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -27,9 +27,7 @@ class ConfigTest extends AbstractContainerConfigTest
             ['Aura\Router\RouterContainer'],
             ['Radar\Adr\Adr'],
             ['Radar\Adr\Handler\ActionHandler'],
-            ['Radar\Adr\Handler\ExceptionHandler'],
             ['Radar\Adr\Handler\RoutingHandler'],
-            ['Radar\Adr\Handler\SendingHandler'],
             ['Radar\Adr\Resolver'],
             ['Radar\Adr\Router\Map'],
         ];
